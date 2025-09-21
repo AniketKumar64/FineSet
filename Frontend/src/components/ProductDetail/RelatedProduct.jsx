455921659970c1668642d23548ed4324286be19b
AniketKumar64 <kumaraniket20805@gmail.com>
2025-09-21 21:37:22 +0530

    fix: correct import path

--- Frontend/src/components/ProductDetail/RelatedProduct.jsx
+++ Frontend/src/components/ProductDetail/RelatedProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../../Context/ShopContext";
+import { ShopContext } from "../../context/ShopContext";
 import ProductCard from "../Shop/ProductCard";
 
 const RelatedProduct = ({ category, subcategory }) => {
