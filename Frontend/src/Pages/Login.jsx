e5b995036f8d70465e5e2d165e4f60a9d00b753a
AniketKumar64 <kumaraniket20805@gmail.com>
2025-09-21 21:26:46 +0530

    fix: remove unnecessary blank line in Login component

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 
 import { toast } from "sonner";
 import { ShopContext } from "../context/ShopContext.jsx";
-
 const Login = () => {
   const [username, setusername] = useState("");
   const [email, setEmail] = useState("");
