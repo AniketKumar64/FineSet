3f4af0e04eab3054068d1298cce6b96ac2cffc77
AniketKumar64 <kumaraniket20805@gmail.com>
2025-09-21 15:39:31 +0530

    feat: remove  component

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import Hero from '../components/Home/Hero'
+
 
 const Home = () => {
   return (
     <div className=" ">
-      <Hero/>
+      
       <div className="h-screen"></div>
     </div>
   )
